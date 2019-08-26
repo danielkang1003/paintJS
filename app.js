5ce8bbae2a2edc52448a7bcca7907d37e0a59a3a
danielkang1003 <danielkang1003@gmail.com>
2019-08-26 14:08:49 +0900

    mend

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
-
+const colors = document.getElementsByClassName("jsColor");
 //실제 pixel modifier에 사이즈를 주어야함
 canvas.width = 700;   //css와 같이 width height 주기.
 canvas.height = 700;
 
-context.strokeStyle ="#2c2c2c";
-context.lineWidth = 2.5;
+context.strokeStyle ="#2c2c2c"; //default color
+context.lineWidth = 2.5;        //default linewidth which is 굵기
 
 let painting = false;
 
@@ -23,20 +23,27 @@ function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;
   if(!painting){
-    console.log("creating path", x, y);
+    // console.log("creating path", x, y);
     context.beginPath();
     context.moveTo(x, y);
   } else{
-    console.log("creating line", x, y);
+    // console.log("creating line", x, y);
     context.lineTo(x, y);
     context.stroke();
   }
 }
 
-function onMouseDown(event){
-  painting = true;
-}
+// function onMouseDown(event){
+//   painting = true;
+// }
 
+function handleColorClick(event){
+  // console.log(event.target.style);
+  const color = event.target.style.backgroundColor;
+  // console.log(color);
+  //override strokeStyle
+  context.strokeStyle = color;
+}
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
@@ -44,3 +51,5 @@ if(canvas){
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
